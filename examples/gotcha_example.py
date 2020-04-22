be935f6234e21da80747d4657cabeb7eb3e56d25
Ronald Kemker <63465692+ron-kemker@users.noreply.github.com>
2020-04-22 08:16:47 -0400

    Update gotcha_example.py
    Added timers to profile the runtime.

--- examples/gotcha_example.py
+++ examples/gotcha_example.py
@@ -8,17 +8,23 @@ Author: Ronald Kemker
 
 from fileIO.gotcha import GOTCHA
 from image_formation import polar_format_algorithm as PFA
-from utils import imshow
+from utils import imshow, Timer
 from signal_processing import taylor_window
 
 data_path ='..\..\data\GOTCHA\DATA\pass8\HH'
-sar_obj = GOTCHA(data_path,
-            center_frequency=9.6e9, 
-            bandwidth=300e6,
-            taper_func=taylor_window,
-            min_azimuth_angle=40,
-            max_azimuth_angle=45
-            )
 
-image = PFA(sar_obj)
+timer = Timer('FileIO')
+with timer as _:
+    sar_obj = GOTCHA(data_path,
+                center_frequency=9.6e9, 
+                bandwidth=300e6,
+                taper_func=taylor_window,
+                min_azimuth_angle=40,
+                max_azimuth_angle=45
+                )
+
+timer = Timer("PFA")
+with timer as _:
+    image = PFA(sar_obj)
+    
 imshow(image.T)
