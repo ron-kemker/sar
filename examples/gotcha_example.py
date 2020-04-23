3e25002e744a0c433e9ff3b1d956804c987d4041
Ronald Kemker <63465692+ron-kemker@users.noreply.github.com>
2020-04-23 09:02:16 -0400

    Update gotcha_example.py
    Added a comparison against using autofocus and not using autofocus.

--- examples/gotcha_example.py
+++ examples/gotcha_example.py
@@ -8,8 +8,10 @@ Author: Ronald Kemker
 
 from fileIO.gotcha import GOTCHA
 from image_formation import polar_format_algorithm as PFA
-from utils import imshow, Timer
+from utils import imshow, Timer, polyphase_interp
 from signal_processing import taylor_window
+import numpy as np
+import matplotlib.pyplot as plt
 
 data_path ='..\..\data\GOTCHA\DATA\pass8\HH'
 
@@ -23,8 +25,31 @@ with timer as _:
                 max_azimuth_angle=45
                 )
 
+plt.figure(1, figsize=[10,10])
+
 timer = Timer("PFA")
 with timer as _:
-    image = PFA(sar_obj)
-    
+    image = PFA(sar_obj, 
+                map_drift=False, 
+                upsample=False, 
+                interp_func = np.interp,
+                # num_range_samples=128,
+                # num_crossrange_samples=256
+                )
+
+plt.subplot(2,1,1)
 imshow(image.T)
+
+timer = Timer("PFA w/ Multi-Aperture Map-Drift")
+with timer as _:
+    image = PFA(sar_obj, 
+                map_drift=True, 
+                upsample=False, 
+                interp_func = np.interp,
+                # num_range_samples=128,
+                # num_crossrange_samples=256
+                )
+
+plt.subplot(2,1,2)
+imshow(image.T)
+
