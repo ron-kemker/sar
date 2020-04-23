561b7ba521df14b768836d5d12e767ae79d7b50a
Ronald Kemker <63465692+ron-kemker@users.noreply.github.com>
2020-04-23 13:49:46 -0400

    Update gotcha_example.py
    Cleaned up the example

--- examples/gotcha_example.py
+++ examples/gotcha_example.py
@@ -13,6 +13,7 @@ from signal_processing import taylor_window
 import numpy as np
 import matplotlib.pyplot as plt
 from autofocus import multi_aperture_map_drift_algorithm as MAM
+from autofocus import phase_gradient_autofocus as PGA
 
 data_path ='..\..\data\GOTCHA\DATA\pass8\HH'
 
@@ -21,35 +22,26 @@ with timer as _:
     sar_obj = GOTCHA(data_path,
                 center_frequency=9.6e9, 
                 bandwidth=300e6,
-                taper_func=taylor_window,
+                # taper_func=taylor_window,
                 min_azimuth_angle=40,
-                max_azimuth_angle=45
+                max_azimuth_angle=42
                 )
 
-plt.figure(1, figsize=[10,10])
+fig, ax = plt.subplots(1, 3, figsize=[12,10])
+txt = ['PFA',"PFA w/ Multi-Aperture Map-Drift",
+       "PFA w/ Phase Gradient Autofocus"]
+af = [None, MAM, PGA]
 
-timer = Timer("PFA")
-with timer as _:
-    image = PFA(sar_obj, 
-                upsample=False, 
-                interp_func = np.interp,
-                # num_range_samples=128,
-                # num_crossrange_samples=256
-                )
+for i in range(len(txt)):
 
-plt.subplot(2,1,1)
-imshow(image.T)
+    timer = Timer(txt[i])
+    with timer as _:
+        image = PFA(sar_obj, 
+                    upsample=False, 
+                    interp_func = np.interp,
+                    auto_focus = af[i]
+                    )
 
-timer = Timer("PFA w/ Multi-Aperture Map-Drift")
-with timer as _:
-    image_mam = PFA(sar_obj, 
-            upsample=False, 
-            interp_func = np.interp,
-            auto_focus = MAM,
-            # num_range_samples=128,
-            # num_crossrange_samples=256
-            )
-
-plt.subplot(2,1,2)
-imshow(image_mam.T)
-
+    imshow(image, ax=ax[i])
+    ax[i].axis('off')
+    ax[i].title.set_text(txt[i])
