231a5c18c6688597189efd2afde722312e92d8c9
Ronald Kemker <63465692+ron-kemker@users.noreply.github.com>
2020-04-24 14:18:34 -0400

    Update gotcha_example.py
    Messing around with the example.

--- examples/gotcha_example.py
+++ examples/gotcha_example.py
@@ -14,22 +14,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from autofocus import multi_aperture_map_drift_algorithm as MAM
 from autofocus import phase_gradient_autofocus as PGA
+from autofocus import spatial_variant_autofocus2 as SVA
 
-data_path ='..\..\data\GOTCHA\DATA\pass8\HH'
+data_path ='..\..\data\GOTCHA\DATA\pass1\VV'
 
 timer = Timer('FileIO')
 with timer as _:
     sar_obj = GOTCHA(data_path,
                 center_frequency=9.6e9, 
-                bandwidth=300e6,
-                taper_func=taylor_window,
-                min_azimuth_angle=40,
-                max_azimuth_angle=42
+                bandwidth=650e6,
+                taper_func=None,
+                min_azimuth_angle=-50,
+                max_azimuth_angle=-46,
                 )
 
 fig, ax = plt.subplots(1, 3, figsize=[12,10])
 txt = ['PFA',"PFA w/ Multi-Aperture Map-Drift",
-       "PFA w/ Phase Gradient Autofocus"]
+        "PFA w/ Phase Gradient Autofocus"]
 af = [None, MAM, PGA]
 
 for i in range(len(txt)):
@@ -39,11 +40,13 @@ for i in range(len(txt)):
         image = PFA(sar_obj, 
                     upsample=False, 
                     interp_func = np.interp,
-                    auto_focus = af[i]
+                    auto_focus = af[i],
+                    single_precision=False,
                     )
 
-    imshow(image, ax=ax[i])
+    imshow(image.T, ax=ax[i])
     ax[i].axis('off')
     ax[i].title.set_text(txt[i])
 
 plt.tight_layout()
+
