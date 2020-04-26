af80511a03c0e55531e487ef1095fc035d4bc210
Ronald Kemker <63465692+ron-kemker@users.noreply.github.com>
2020-04-25 21:49:57 -0400

    Update gotcha_example.py
    Additional  mods to example file.

--- examples/gotcha_example.py
+++ examples/gotcha_example.py
@@ -16,14 +16,14 @@ from autofocus import multi_aperture_map_drift_algorithm as MAM
 from autofocus import phase_gradient_autofocus as PGA
 from autofocus import spatial_variant_autofocus2 as SVA
 
-data_path ='..\..\data\GOTCHA\DATA\pass1\VV'
+data_path ='..\..\data\GOTCHA\DATA\pass1\HH'
 
 timer = Timer('FileIO')
 with timer as _:
     sar_obj = GOTCHA(data_path,
                 center_frequency=9.6e9, 
                 bandwidth=650e6,
-                taper_func=None,
+                taper_func=taylor_window,
                 min_azimuth_angle=-50,
                 max_azimuth_angle=-46,
                 )
@@ -38,10 +38,10 @@ for i in range(len(txt)):
     timer = Timer(txt[i])
     with timer as _:
         image = PFA(sar_obj, 
-                    upsample=False, 
+                    upsample=True, 
                     interp_func = np.interp,
                     auto_focus = af[i],
-                    single_precision=False,
+                    single_precision=True,
                     )
     r = i // 2
     c = i % 2
@@ -50,7 +50,7 @@ for i in range(len(txt)):
     ax[c,r].title.set_text(txt[i])
 
     if i == 0:
-        timer = Timer('PFA w/ Spatially Variant Autofocus')
+        timer = Timer('Spatially Variant Autofocus')
         with timer as _:
             image_af = SVA(image)
         imshow(image_af.T, ax=ax[1,1])
