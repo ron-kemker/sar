be496169216df2d28497a2fc2b7c2e7249504968
Ronald Kemker <63465692+ron-kemker@users.noreply.github.com>
2020-04-27 20:04:03 -0400

    Update signal_processing.py
    Added SVA

--- signal_processing.py
+++ signal_processing.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 '''
 File: signal_processing.py
-Description: Contains SAR signal processing tools
+Description: Contains SAR signal processing tools, mostly sidelobe management
 Author: Ronald Kemker
 
 '''
 
 import numpy as np
+from scipy.signal import convolve2d as conv2d
 
 # This applies a Hamming window to the CPHD file (side-lobe suppression)
 def hamming_window(cphd):
@@ -74,3 +75,148 @@ def taylor(N, sidelobe, n_bar=None):
     
     w = w/w.max()          
     return w
+
+def spatial_variant_apodization(X, N=2):
+    '''
+    This runs the spatial variant apodization algorithm 
+    This matches a version shared by a co-worker.  
+
+    Parameters
+    ----------
+    X : complex-valued, numeric.  This is the input image.
+    N : integer >=1.  This is the sampling frequency.
+        DESCRIPTION. The default is 2.
+
+    Returns
+    -------
+    output : numeric.  This is the output image.
+    
+    References
+    ----------
+        - Carrera, Goodman, and Majewski (1995), Appendix D
+    '''
+    # Define functions to clean up space    
+    min2D = np.minimum
+    max2D = np.maximum
+    
+    X2 = np.rot90(X)
+
+    filt = np.zeros((1, 2*N + 1), np.float32)
+    filt[0,0] = 1
+    filt[0,-1] = 1
+
+    # Horizontal Apodization
+    sum_r = conv2d(X.real, filt, mode='same')
+    sum_i = conv2d(X.imag, filt, mode='same')
+    
+    weights = np.zeros(sum_r.shape, X.dtype)
+    idx = sum_r != 0
+    weights.real[idx] = min2D(max2D(-X.real[idx]/sum_r[idx], 
+                                    0.0), 0.5)
+    idx = sum_i != 0
+    weights.imag[idx] = min2D(max2D(-X.imag[idx]/sum_i[idx], 
+                                    0.0), 0.5)
+    
+    X1 = X.real+sum_r*weights.real + (X.imag+sum_i*weights.imag) * 1j 
+             
+    det1 = X1 * np.conj(X1)
+
+    # Vertical Apodization
+    sum_r = conv2d(X2.real, filt, mode='same')
+    sum_i = conv2d(X2.imag, filt, mode='same')
+    
+    weights = np.zeros(sum_r.shape, X.dtype)
+    idx = sum_r != 0
+    weights.real[idx] = min2D(max2D(-X2.real[idx]/sum_r[idx], 
+                                    0.0), 0.5)
+    idx = sum_i != 0
+    weights.imag[idx] = min2D(max2D(-X2.imag[idx]/sum_i[idx], 
+                                    0.0), 0.5)
+    
+    X2 = X2.real+sum_r*weights.real + (X2.imag+sum_i*weights.imag) * 1j 
+             
+    det2 = X2 * np.conj(X2)
+    
+    X2 = np.rot90(X2, 3)
+    det2 = np.rot90(det2, 3)
+    
+    output = np.zeros(X1.shape, X1.dtype)
+    idx = det1 < det2
+    output[idx] = X1[idx]
+    output[~idx] = X2[~idx]
+    
+    return output
+
+def spatial_variant_apodization2(X, N=1):
+    '''
+    This runs the spatial variant apodization algorithm 
+    This is more similar to the book implementation.
+
+    Parameters
+    ----------
+    X : complex-valued, numeric.  This is the input image.
+    N : integer >=1.  This is the sampling frequency.
+        DESCRIPTION. The default is 2.
+
+    Returns
+    -------
+    output : numeric.  This is the output image.
+    
+    References
+    ----------
+        - Carrera, Goodman, and Majewski (1995), Appendix D
+    '''
+    X1 = np.copy(X)
+    X2 = np.rot90(X)
+
+    filt = np.zeros((1, 2*N + 1), np.float32)
+    filt[0,0] = 1
+    filt[0,-1] = 1
+
+    # Horizontal Apodization
+    sum_r = conv2d(X.real, filt, mode='same')
+    sum_i = conv2d(X.imag, filt, mode='same')
+    
+    w_u = np.zeros(sum_r.shape, X.dtype)
+    w_u.real = -X.real/sum_r
+    w_u.imag = -X.imag/sum_i
+    
+    X1 = np.copy(X)
+    idx_r = np.logical_and(w_u.real >= 0.0, w_u.real<= 0.5) 
+    idx_i = np.logical_and(w_u.imag >= 0.0, w_u.imag<= 0.5)
+    X1.real[idx_r] = 0.0
+    X1.imag[idx_i] = 0.0
+    
+    idx_r = w_u.real > 0.5
+    idx_i = w_u.imag > 0.5
+    X1.real[idx_r] = X.real[idx_r]+sum_r[idx_r]*0.5
+    X1.imag[idx_i] = X.imag[idx_i]+sum_i[idx_i]*0.5            
+    det1 = X1 * np.conj(X1)
+
+    # Vertical Apodization
+    sum_r = conv2d(X2.real, filt, mode='same')
+    sum_i = conv2d(X2.imag, filt, mode='same')
+    
+    w_u = np.zeros(sum_r.shape, X.dtype)
+    w_u.real = -X2.real/sum_r
+    w_u.imag = -X2.imag/sum_i
+    
+    idx_r = np.logical_and(w_u.real >= 0.0, w_u.real<= 0.5) 
+    idx_i = np.logical_and(w_u.imag >= 0.0, w_u.imag<= 0.5)
+    X2.real[idx_r] = 0.0
+    X2.imag[idx_i] = 0.0 
+    
+    idx_r = w_u.real > 0.5
+    idx_i = w_u.imag > 0.5
+    X2.real[idx_r] = X2.real[idx_r]+sum_r[idx_r]*0.5
+    X2.imag[idx_i] = X2.imag[idx_i]+sum_i[idx_i]*0.5         
+    det2 = X2 * np.conj(X2)
+    
+    X2 = np.rot90(X2, 3)
+    det2 = np.rot90(det2, 3)
+    
+    output = np.zeros(X1.shape, X1.dtype)
+    idx = det1 < det2
+    output[idx] = X1[idx]
+    output[~idx] = X2[~idx]
+    return output
