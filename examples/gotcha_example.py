12a5c70647636813a72ebaae1cee043cc57e3563
Ronald Kemker <63465692+ron-kemker@users.noreply.github.com>
2020-05-19 18:35:04 -0400

    Update gotcha_example.py
    Fixed based on updates to PFA

--- examples/gotcha_example.py
+++ examples/gotcha_example.py
@@ -6,26 +6,26 @@ Author: Ronald Kemker
 
 '''
 
+import numpy as np
 from fileIO.gotcha import GOTCHA
 from image_formation import polar_format_algorithm as PFA
-from utils import imshow, Timer, polyphase_interp
+from utils import imshow, Timer
 from signal_processing import spatial_variant_apodization as SVA
-import numpy as np
 import matplotlib.pyplot as plt
 from autofocus import phase_gradient_autofocus as PGA
 from signal_processing import residual_video_phase_compensation as RVP_Comp
 
 data_path ='..\..\data\GOTCHA\DATA\pass1\HH'
+dynamic_range = 30
 plt.close('all')
-
 
 timer = Timer('FileIO')
 with timer as _:
     sar_obj = GOTCHA(data_path,
                 center_frequency=9.6e9, 
                 bandwidth=650e6,
-                min_azimuth_angle=-50,
-                max_azimuth_angle=-46,
+                min_azimuth_angle = 15,
+                max_azimuth_angle = 19,
                 )
 fig, ax = plt.subplots(1,3 , figsize=[8,8])
 
@@ -35,11 +35,12 @@ sar_obj.cphd = RVP_Comp(sar_obj.cphd, sar_obj.freq)
 timer = Timer('Polar Format Algorithm')
 with timer as _:
     image = PFA(sar_obj, 
-            upsample=True, 
-            # interp_func = np.interp,
             single_precision=True,
-            )    
-    imshow(image.T, ax=ax[0])
+            upsample=True,
+            # taylor_weighting=30,
+            )
+    # imshow(image, 45)    
+    imshow(image, dynamic_range, ax=ax[0])
     ax[0].axis('off')
     ax[0].title.set_text('Polar Format Algorithm')
 
@@ -47,14 +48,14 @@ with timer as _:
 timer = Timer('Spatially Variant Apodization')
 with timer as _:
     image_sva = SVA(image, N=1)
-    imshow(image_sva.T, ax=ax[1])
+    imshow(image_sva, dynamic_range, ax=ax[1])
     ax[1].axis('off')
     ax[1].title.set_text('Spatially Variant Apodization')
 
 timer = Timer('Phase Gradient Autofocus')
 with timer as _:
-    image_af = PGA(image_sva, range_subset=100)
-    imshow(image_af.T, ax=ax[2])
+    image_af, ph_error = PGA(image_sva)
+    imshow(image_af, dynamic_range, ax=ax[2])
     ax[2].axis('off')
     ax[2].title.set_text('Phase Gradient Autofocus')
 
