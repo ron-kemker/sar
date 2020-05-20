fb827da7986054c589b44ad43f52ee4b5af85d69
Ronald Kemker <63465692+ron-kemker@users.noreply.github.com>
2020-05-20 11:13:15 -0400

    Update interpolation_comparison.py
    Added Cubic Interpolation for comparison as well as added multiprocessing support for PFA (since polyphase is slow)

--- examples/interpolation_comparison.py
+++ examples/interpolation_comparison.py
@@ -19,43 +19,45 @@ def cubic_interp(x, xp, yp, left=0, right=0):
                              fill_value=(left,right), bounds_error=False)
     return f(x)
 
-
-data_path ='..\..\data\Sandia\\'
-plt.close('all')
-
-timer = Timer('FileIO')
-with timer as _:
-    sar_obj = SANDIA(data_path)
+if __name__ == '__main__':
     
-fig, ax = plt.subplots(1,2, figsize=[8,8])
-plt.tight_layout()
+    data_path ='..\..\data\Sandia\\'
+    plt.close('all')
+    
+    timer = Timer('FileIO')
+    with timer as _:
+        sar_obj = SANDIA(data_path)
+        
+    fig, ax = plt.subplots(1,2, figsize=[8,8])
+    plt.tight_layout()
+    
+    sar_obj.cphd = RVP_Comp(sar_obj.cphd, sar_obj.freq, sar_obj.chirprate,
+                            sar_obj.delta_r)
+    
+    timer = Timer('Cubic Interpolation')
+    with timer as _:
+        image = PFA(sar_obj, 
+                interp_func=cubic_interp,
+                single_precision=True,
+                taylor_weighting=30,
+                )
+    
+        imshow(image, ax=ax[0], dynamic_range=45)
+        ax[0].axis('off')
+        ax[0].title.set_text('Cubic Interpolation')
+    
+    timer = Timer('Polyphase Interpolation')
+    with timer as _:
+        image = PFA(sar_obj, 
+                single_precision=True,
+                taylor_weighting=30,
+                n_jobs=8,
+                )
+        imshow(image, ax=ax[1], dynamic_range=45)
+        ax[1].axis('off')
+        ax[1].title.set_text('Polyphase Interpolation')
 
-sar_obj.cphd = RVP_Comp(sar_obj.cphd, sar_obj.freq, sar_obj.chirprate,
-                        sar_obj.delta_r)
-
-timer = Timer('Cubic Interpolation')
-with timer as _:
-    image = PFA(sar_obj, 
-            interp_func=cubic_interp,
-            single_precision=True,
-            taylor_weighting=30,
-            )
-
-    imshow(image, ax=ax[0], dynamic_range=45)
-    ax[0].axis('off')
-    ax[0].title.set_text('Cubic Interpolation')
-
-timer = Timer('Polyphase Interpolation')
-with timer as _:
-    image = PFA(sar_obj, 
-            single_precision=True,
-            taylor_weighting=30,
-            )
-    imshow(image, ax=ax[1], dynamic_range=45)
-    ax[1].axis('off')
-    ax[1].title.set_text('Polyphase Interpolation')
-
-
+    
 
 
 
