29773a4534b4497e9e7becbd7322628fe8a90806
Ronald Kemker <63465692+ron-kemker@users.noreply.github.com>
2020-05-20 11:40:11 -0400

    Update gotcha_example.py
    Updated example for multiprocessor support.

--- examples/gotcha_example.py
+++ examples/gotcha_example.py
@@ -15,47 +15,46 @@ import matplotlib.pyplot as plt
 from autofocus import phase_gradient_autofocus as PGA
 from signal_processing import residual_video_phase_compensation as RVP_Comp
 
-data_path ='..\..\data\GOTCHA\DATA\pass1\HH'
-dynamic_range = 30
-plt.close('all')
+if __name__ == '__main__':
 
-timer = Timer('FileIO')
-with timer as _:
-    sar_obj = GOTCHA(data_path,
-                center_frequency=9.6e9, 
-                bandwidth=650e6,
-                min_azimuth_angle = 15,
-                max_azimuth_angle = 19,
+    data_path ='..\..\data\GOTCHA\DATA\pass1\HH'
+    dynamic_range = 30
+    plt.close('all')
+    
+    timer = Timer('FileIO')
+    with timer as _:
+        sar_obj = GOTCHA(data_path,
+                    center_frequency=9.6e9, 
+                    bandwidth=650e6,
+                    min_azimuth_angle = 15,
+                    max_azimuth_angle = 19,
+                    )
+    fig, ax = plt.subplots(1,3 , figsize=[8,8])
+    
+    sar_obj.cphd = RVP_Comp(sar_obj.cphd, sar_obj.freq)
+    
+    timer = Timer('Polar Format Algorithm')
+    with timer as _:
+        image = PFA(sar_obj, 
+                single_precision=True,
+                n_jobs=8,
                 )
-fig, ax = plt.subplots(1,3 , figsize=[8,8])
-
-sar_obj.cphd = RVP_Comp(sar_obj.cphd, sar_obj.freq)
-
-
-timer = Timer('Polar Format Algorithm')
-with timer as _:
-    image = PFA(sar_obj, 
-            single_precision=True,
-            upsample=True,
-            # taylor_weighting=30,
-            )
-    # imshow(image, 45)    
-    imshow(image, dynamic_range, ax=ax[0])
-    ax[0].axis('off')
-    ax[0].title.set_text('Polar Format Algorithm')
-
-
-timer = Timer('Spatially Variant Apodization')
-with timer as _:
-    image_sva = SVA(image, N=1)
-    imshow(image_sva, dynamic_range, ax=ax[1])
-    ax[1].axis('off')
-    ax[1].title.set_text('Spatially Variant Apodization')
-
-timer = Timer('Phase Gradient Autofocus')
-with timer as _:
-    image_af, ph_error = PGA(image_sva)
-    imshow(image_af, dynamic_range, ax=ax[2])
-    ax[2].axis('off')
-    ax[2].title.set_text('Phase Gradient Autofocus')
+        imshow(image, dynamic_range, ax=ax[0])
+        ax[0].axis('off')
+        ax[0].title.set_text('Polar Format Algorithm')
+    
+    
+    timer = Timer('Spatially Variant Apodization')
+    with timer as _:
+        image_sva = SVA(image, N=1)
+        imshow(image_sva, dynamic_range, ax=ax[1])
+        ax[1].axis('off')
+        ax[1].title.set_text('Spatially Variant Apodization')
+    
+    timer = Timer('Phase Gradient Autofocus')
+    with timer as _:
+        image_af, ph_error = PGA(image_sva, win_params=[10,0.5])
+        imshow(image_af, dynamic_range, ax=ax[2])
+        ax[2].axis('off')
+        ax[2].title.set_text('Phase Gradient Autofocus')
 
